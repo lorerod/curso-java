21c49232dee977f1fd23c66754ebb06af0e640e9
Maria Lorena Rodriguez <Maria.Rodriguez@personal.com.py>
2016-08-29 11:05:29 -0400

    Primer Commit

---
+++ proyecto/src/main/java/py/com/personal/bs/App.java
@@ -0,0 +1,13 @@
+package py.com.personal.bs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
